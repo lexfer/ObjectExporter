a58e25649debc728636f80acb93dabcff4a48212
Alexey <lexfer.mail@gmail.com>
2017-03-22 13:04:23 +0700

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+
+from configparser import SafeConfigParser
 import socket
 import ssl
+
+parser = SafeConfigParser()
+parser.read('config.ini')
+
+print(parser.get('server', 'ip'))
+
+
+
+
+
+
+
+
+
+
+
 
