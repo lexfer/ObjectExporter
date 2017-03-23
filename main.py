3d4853b638739c54a15a792c4406786734a6c2f9
Alexey <lexfer.mail@gmail.com>
2017-03-23 08:59:25 +0700

    first commit

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ import ssl
 
 parser = SafeConfigParser()
 parser.read('config.ini')
+server_ip = parser.get('server', 'ip')
+server_port = int(parser.get('server', 'port'))
 
-print(parser.get('server', 'ip'))
+certfile = parser.get('ssl','ssl_certfile')
+print(certfile)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+'''
+ssl_sock = ssl.wrap_socket(sock,
+                           ca_certs=certfile,
+                           cert_reqs=ssl.CERT_REQUIRED)
 
-
-
-
-
-
-
-
-
-
-
-
+ssl_sock.connect((server_ip, server_port))
+'''
+context = ssl.create_default_context()
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+context.verify_mode = ssl.CERT_REQUIRED
+context.check_hostname = True
+context.load_verify_locations(certfile)
